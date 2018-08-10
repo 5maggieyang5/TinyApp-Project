80294c9d216edcf3d3b2b12d2a8989f534a542af
mag <mag.it.use@gmail.com>
2018-08-10 02:23:14 +0000

    added logout button beside user's name, added logic in header.ejs file: when there's user login, show user name & Logout button; once user click logout, show the login form. also added the route for logout on server file: once logout clicked, clear the cookie and redirect to /urls page

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express      = require("express");
 const app          = express();
-const PORT         = 8080; // default port 8080
+const PORT         = process.env.PORT || 8080; // default port 8080
 const bodyParser   = require("body-parser");
 const cookieParser = require('cookie-parser');
 const users        = [];
@@ -78,6 +78,11 @@ app.post("/login", (req, res) => {
   users.push(user);
   console.log(users);
   res.cookie("username", user.username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
   res.redirect("/urls");
 });
 
